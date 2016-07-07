1d8ff0d50b9067802ae529249a1cb2af1fb5b64f
Mike <mdmccarthyuk@gmail.com>
2016-07-07 16:01:23 +0100

    First commit

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name 'mm-jenkins'
-maintainer 'The Authors'
+maintainer 'Mike McCarthy'
 maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures mm-jenkins'
 long_description 'Installs/Configures mm-jenkins'
 version '0.1.0'
+
+depends 'tomcat'
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,6 @@
 # Cookbook Name:: mm-jenkins
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Mike McCarthy, All Rights Reserved.
+
+
